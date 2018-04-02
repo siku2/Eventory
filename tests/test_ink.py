b3b2f77075b9813ed17e56bd08a96027a8b945a9
Simon Berger <siku2@outlook.de>
2018-04-02 21:25:45 +0200

    it finally worked

--- tests/test_ink.py
+++ tests/test_ink.py
@@ -1,19 +1,19 @@
 import asyncio
 
+import pytest
+
 import eventory
-from eventory import Eventoriment
 
 
-async def run_intercept():
+@pytest.mark.asyncio
+async def test__intercept():
     eventory.load_ext("inktory")
     with open("tests/the_intercept.evory", "r") as f:
         story = eventory.load(f)
-        story.meta.requirements = [Eventoriment("requests")]
-        narrator = eventory.StreamEventarrator()
-        instructor = story.narrate(narrator)
-        await instructor.play()
+    narrator = eventory.StreamEventarrator()
+    instructor = story.narrate(narrator)
 
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run_intercept())
+    loop.run_until_complete(test__intercept())
