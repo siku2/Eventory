7c8057e057a8efeea663e4b12a83da1f189afca5
Simon Berger <siku2@outlook.de>
2018-04-02 23:15:12 +0200

    pray

--- eventory/__version__.py
+++ eventory/__version__.py
@@ -1,7 +1,7 @@
 __title__ = "Eventory"
 __description__ = "Story time!"
 __url__ = "https://github.com/siku2/Eventory"
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __author__ = "siku2"
 __author_email__ = "siku2@outlook.com"
 __license__ = "MIT"
