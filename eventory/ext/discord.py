4e95f02d6441d30ee56d562344e8cf23eb0d43c1
Simon Berger <siku2@outlook.de>
2018-04-05 13:03:38 +0200

    starting with the doc

--- eventory/ext/discord.py
+++ eventory/ext/discord.py
@@ -1,20 +1,25 @@
-from typing import Union
+import inspect
+from typing import Callable, Coroutine, Union
 
 import discord
+from discord.ext.commands import Bot
 
 from eventory import Eventarrator
 
 
 class DiscordEventarrator(Eventarrator):
 
-    def __init__(self, client: discord.Client, channel: Union[discord.TextChannel, discord.DMChannel]):
+    def __init__(self, client: discord.Client, channel: Union[discord.TextChannel, discord.DMChannel],
+                 message_check: Callable[[discord.Message], Union[bool, Coroutine]] = None, **kwargs):
         self.client = client
         self.channel = channel
+
+        self.message_check = message_check
 
     async def output(self, out: str):
         await self.channel.send(out)
 
-    def input_check(self, msg: discord.Message) -> bool:
+    def input_filter(self, msg: discord.Message) -> bool:
         if self.channel.id != msg.channel.id:
             return False
         if self.client._connection.is_bot:  # bots should ignore themselves
@@ -23,6 +28,26 @@ class DiscordEventarrator(Eventarrator):
 
         return True
 
+    async def input_check(self, msg: discord.Message) -> bool:
+        if self.message_check:
+            ret = self.message_check(msg)
+            if inspect.iscoroutine(ret):
+                ret = await ret
+            return ret
+
+        if isinstance(self.client, Bot):
+            ctx = await self.client.get_context(msg)
+            if ctx.command:
+                return False
+
     async def input(self) -> str:
-        msg = await self.client.wait_for("message", check=self.input_check)
+        while True:
+            msg = await self.client.wait_for("message", check=self.input_check)
+            if await self.input_check(msg):
+                break
         return msg.content
+
+
+class EventoryCog:
+    def __init__(self, bot: Bot):
+        self.bot = bot
